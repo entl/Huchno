fcf85c37e0a93e7a953018a126573d9bec1e2a9c
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-12-18 21:28:15 +0200

    update router created

--- api/user.py
+++ api/user.py
@@ -4,7 +4,7 @@ from loguru import logger
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
-from app.user.schemas import UserCreate, UserOut
+from app.user.schemas import UserCreate, UserOut, UserUpdate
 from app.user.service import UserService
 
 from core import exceptions
@@ -53,4 +53,10 @@ async def get_user_by_id(user_id: str, user_service: Annotated[UserService, Depe
 @users_router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserOut)
 async def create_user(user_service: Annotated[UserService, Depends()], user: UserCreate):
     user: UserOut = await user_service.create_user(user=user)
+    return user
+
+
+@users_router.patch("/{user_id}", response_model=UserOut)
+async def update_user(user_service: Annotated[UserService, Depends()], user: UserUpdate, user_id: str):
+    user: UserOut = await user_service.update_user(user)
     return user
