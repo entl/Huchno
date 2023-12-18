f5523e0493e4da555dc40f1ccf48eff901b5a604
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-12-18 21:26:13 +0200

    age invalid exception added

--- core/exceptions/user.py
+++ core/exceptions/user.py
@@ -1,25 +1,32 @@
 from core.exceptions.base import CustomException
+from fastapi import status
 
 
 class PasswordDoesNotMatchException(CustomException):
-    code = 401
+    code = status.HTTP_401_UNAUTHORIZED
     error_code = "USER__PASSWORD_DOES_NOT_MATCH"
     message = "password does not match"
 
 
 class DuplicateEmailOrNicknameException(CustomException):
-    code = 400
+    code = status.HTTP_409_CONFLICT
     error_code = "USER__DUPLICATE_EMAIL_OR_NICKNAME"
     message = "duplicate email or nickname"
 
 
 class UserNotFoundException(CustomException):
-    code = 404
+    code = status.HTTP_404_NOT_FOUND
     error_code = "USER__NOT_FOUND"
     message = "user not found"
 
 
 class UserNotVerified(CustomException):
-    code = 400
+    code = status.HTTP_403_FORBIDDEN
     error_code = "USER__NOT_VERIFIED"
     message = "user not verified"
+
+
+class UserAgeInvalid(CustomException):
+    code = status.HTTP_409_CONFLICT
+    error_code = "USER__AGE_INVALID"
+    message = "user age invalid"
