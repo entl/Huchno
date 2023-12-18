d42fdf84d91ebcd35b510c1b247db2c8c50faead
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-12-19 01:15:16 +0200

    docstring added

--- api/user.py
+++ api/user.py
@@ -53,14 +53,45 @@ async def get_user_by_id(user_id: UUID4, user_service: Annotated[UserService, De
 
 @users_router.post("/", response_model=UserOut, status_code=status.HTTP_201_CREATED,)
 async def create_user(user_service: Annotated[UserService, Depends()], user: UserCreate):
+    """
+    Create a new user.
+
+    Args:
+        user_service (UserService): User Service instance.
+        user (UserCreate): User data to create.
+
+    Returns:
+        UserOut: Created user data.
+    """
     return await user_service.create_user(user=user)
 
 
 @users_router.patch("/{user_id}", response_model=UserOut, status_code=status.HTTP_200_OK)
 async def update_user(user_service: Annotated[UserService, Depends()], user: UserUpdate, user_id: UUID4):
+    """
+    Update a user.
+
+    Args:
+        user_service (UserService): User Service instance.
+        user (UserUpdate): User data to update.
+        user_id (str): The ID of the user to update.
+
+    Returns:
+        UserOut: Updated user data.
+    """
     return await user_service.update_user(user)
 
 
 @users_router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(user_service: Annotated[UserService, Depends()], user_id: UUID4):
+    """
+    Delete a user.
+
+    Args:
+        user_service (UserService): User Service instance.
+        user_id (str): The ID of the user to delete.
+
+    Returns:
+        None
+    """
     await user_service.delete_user(user_id=user_id)
