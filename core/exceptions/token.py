540912d71dd05711863572862ac3f5db0ab9fae3
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-12-22 16:44:40 +0200

    created class for token exceptions

--- core/exceptions/token.py
+++ core/exceptions/token.py
@@ -1,13 +1,17 @@
 from core.exceptions.base import CustomException
 
 
-class DecodeTokenException(CustomException):
+class TokenException(CustomException):
+    pass
+
+
+class DecodeTokenException(TokenException):
     code = 400
     error_code = "TOKEN__DECODE_ERROR"
     message = "token decode error"
 
 
-class ExpiredTokenException(CustomException):
+class ExpiredTokenException(TokenException):
     code = 400
     error_code = "TOKEN__EXPIRE_ERROR"
     message = "expired token"
