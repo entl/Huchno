e81c0619a899bb816a32518b699114851302041f
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-12-23 00:11:15 +0200

    print deleted

--- api/user.py
+++ api/user.py
@@ -135,7 +135,6 @@ async def delete_user(
     Returns:
         None
     """
-    print(current_user.permissions)
     if current_user.id == user_id or Permissions.IsAdmin in current_user.permissions:
         await user_service.delete_user(user_id=user_id)
     else:
