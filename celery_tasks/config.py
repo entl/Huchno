c4a3eaba16de066103c915be34fef2fb28bf20c2
Maksym Vorobyov <maxvorobyov01@gmail.com>
2024-01-09 03:05:37 +0200

    redis url fix

--- celery_tasks/config.py
+++ celery_tasks/config.py
@@ -1,11 +1,9 @@
-import asyncio
-
 from celery import Celery
 
 from core.config import settings
 
-redis_celery_tasks_url = f"redis://{settings.redis_host}:{settings.redis_port}/{settings.redis_celery_db}"
-redis_celery_tasks_backend = f"redis://{settings.redis_host}:{settings.redis_port}/1"
+redis_celery_tasks_url = f"redis://{settings.redis_host}:{settings.redis_port}/{settings.redis_celery_broker_db}"
+redis_celery_tasks_backend = f"redis://{settings.redis_host}:{settings.redis_port}/{settings.redis_celery_backend_db}"
 
 
 celery = Celery('tasks', broker=redis_celery_tasks_url, backend=redis_celery_tasks_backend)
